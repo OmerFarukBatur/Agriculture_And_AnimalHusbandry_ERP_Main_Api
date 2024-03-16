6545a3b906bca7192ef6481fbcb42fa1b26ea8b0
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2024-03-16 12:11:54 +0300

    create base repository

---
+++ Infrastructure/Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,56 @@
+﻿using Application.Repositories;
+using Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts;
+using System.Linq.Expressions;
+
+namespace Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        private readonly AgricultureAndAnimalHusbandryWebAPIDbContext _context;
+
+        public ReadRepository(AgricultureAndAnimalHusbandryWebAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            var query = Table.Where(method);
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = Table.AsNoTracking();
+            return await Table.FirstOrDefaultAsync(method);
+        }
+
+
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = Table.AsNoTracking();
+            return await query.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        }
+        // => await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id)); // benzer işlemi yapmaktadır. 
+        //=> await Table.FindAsync(Guid.Parse(id));
+    }
+}
